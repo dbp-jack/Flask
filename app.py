cd6f3fa7abf7185b41487988b237867bcc5d2564
Minsoo-Jeong <149581532+dbp-jack@users.noreply.github.com>
2024-01-05 14:46:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+if __name__ == '__main__':
+    # This block will only be executed if the script is run directly (not imported)
+    # Additional code for running the Flask application
+    app.run('0.0.0.0', 5000, debug=True)
